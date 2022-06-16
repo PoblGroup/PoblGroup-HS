35d9a637a6500821e49a81a485ad81b55a8d5a41
Rich Griffiths <rich.griffiths89@gmail.com>
2022-06-16 11:40:29 +0100

    Incorrect redirect uri

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -3,7 +3,7 @@ export const msalconfig = {
         clientId: "d30e66e5-5d45-4beb-b6b2-7b37887e332b",
         authority: "https://login.microsoftonline.com/5f0d9160-6b93-41c6-8db2-153e0d7f7960",
         // redirectUri: "http://localhost:3000"
-        redirectUri: "https://main--gorgeous-flan-0ba665.netlify.app"
+        redirectUri: "https://gorgeous-flan-0ba665.netlify.app/"
     },
     cache: {
         cacheLocation: "sessionStorage",
